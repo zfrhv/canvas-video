21a78489902e9806c01b9dd6d75636b7ece461fd
Zahar Pecherichny <zfrhv2010@gmail.com>
2022-03-20 20:36:34 +0200

    a

--- public/animation.js
+++ public/animation.js
@@ -67,9 +67,13 @@ function create_threejs(looping_animation, animation_main) {
   controls.enableZoom = true;
 
   // add light
-  //new THREE.DirectionalLight( 0xffffff, 0.5 )
-  scene.add(new THREE.HemisphereLight(0xf6e86d, 0x404040, 0.5));
-  scene.add(new THREE.HemisphereLight(0x404040, 0xFFFFFF, 0.5));
+  const toplight = new THREE.HemisphereLight(0x86cf66, 0x26401b, 0.5);
+  toplight.position.set(0, 1, 0).normalize();
+  scene.add(toplight);
+
+  const frontLight = new THREE.DirectionalLight(0x4a96df, 0x183048, 0.5);
+  frontLight.position.set(0, 0, 1).normalize();
+  scene.add(frontLight);
 
   const objLoader = new OBJLoader();
   objLoader.load(looping_animation.getAttribute("path"), (object) => {
